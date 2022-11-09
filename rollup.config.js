1b756a2c0aeb47aafac9b42534452c79f2be27e3
Kristján Oddsson <koddsson@gmail.com>
2022-11-09 21:58:37 +0100

    Resolve node_modules in production build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import html from '@web/rollup-plugin-html'
 import esbuild from 'rollup-plugin-esbuild'
+import {nodeResolve} from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'index.html',
   output: {dir: 'dist'},
-  plugins: [esbuild(), html({input: 'index.html'})]
+  plugins: [nodeResolve(), esbuild(), html({input: 'index.html'})]
 }
