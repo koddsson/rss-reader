8cda9f59364b4bbd0e998193de361c50ffb0740c
Kristján Oddsson <koddsson@gmail.com>
2023-11-09 20:24:08 +0000

    register service worker correctly

--- src/js/register-service-worker.ts
+++ src/js/register-service-worker.ts
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
   try {
-    const registration = await navigator.serviceWorker.register('/service-worker.js')
+    const registration = await navigator.serviceWorker.register('./service-worker.js')
     console.log('Service worker registration succeeded:', registration)
   } catch (error) {
     console.error(`Service worker registration failed: ${error}`)
